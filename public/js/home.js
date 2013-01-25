c9fc7d30b29697ddeb94b21282ab108c130ac5f6
Jose F. Romaniello <jfromaniello@gmail.com>
2013-01-25 16:35:36 -0300

    work in progress

--- public/js/home.js
+++ public/js/home.js
@@ -4,4 +4,13 @@ $('#sign-up-btn').on('click', function (e) {
   $('.overlay')
     .show()
     .addClass('active');
+
+});
+
+$('.close').click(function (e) {
+  e.preventDefault();
+  $('#sign-up').removeClass('active');
+  $('.overlay')
+    .hide()
+    .removeClass('active');  
 });
